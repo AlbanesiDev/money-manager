f7946d289dfc8d4906acbb75771c6bcc42bd72b5
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-07-12 23:22:40 -0300

    feat: Function to create a document in firestore from the uid

---
+++ src/presentation/utils/createUserDoc.ts
@@ -0,0 +1,7 @@
+import { doc, setDoc } from "firebase/firestore";
+import { firestore } from "../../config/firebase.config";
+
+export function createUserDoc(uid: string) {
+  const docRef = doc(firestore, `users/${uid}`);
+  setDoc(docRef, { id: uid });
+}
